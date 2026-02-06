e0619ef43638941c15241afb078b3e8e34efd080
Arvind Vishwakarma <arvind.vishwakarma@zennovate.in>
2026-02-06 16:20:41 +0530

    accessing child route in parent component

--- bookStoreApp/src/app/user/user.ts
+++ bookStoreApp/src/app/user/user.ts
@@ -7,8 +7,22 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
-export class User {
+export class User implements OnInit {
   deleteBook: number = 100; // Example book ID to delete, you can replace it with dynamic data
   userId: number = 1; // Example user ID, you can replace it with dynamic data
   authorName: string = 'Arvind'; // Example author name, you can replace it with dynamic data
+  bookId: number= 0;
+  rAuthorName: string = '';
+
+  constructor(private routes: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.routes.firstChild?.params.subscribe((params) => {
+      this.bookId = params['bookId'];
+      this.rAuthorName = params['authorName'];
+    });
+  }
+
 }
+
+
